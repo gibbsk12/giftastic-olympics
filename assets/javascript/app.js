93e7044d021a5ba5eda83f506e31853c65a87b72
Kate Gibbs <kategibbs@Kates-MacBook-Pro-2.local>
2018-06-07 22:45:21 -0400

    add a sport button works

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,6 @@
 var sports = ["SWIMMING", "GYMNASTICS", "FIGURE SKATING", "BEACH VOLLEYBALL", "FENCING", "CURLING"]
 var sportsDiv = $("#sportButtons")
-
-
+//Makes a button for each sport in the array 
 function makeButtons(){
 	sportsDiv.empty();
 	for (var i = 0; i < sports.length; i++){
@@ -13,7 +12,13 @@ function makeButtons(){
 		sportsDiv.append(newButton);
 	}
 }
-
-
+//Calls the function so buttons appear on page load 
 makeButtons();
-
+//When the user clicks add sport button, adds the sport to the array and recreates buttons
+$("#addSport").on("click", function (event) {
+	event.preventDefault();
+	var newSport = $("#sport-input").val().trim().toUpperCase();
+	sports.push(newSport);
+	makeButtons();
+});
+//
